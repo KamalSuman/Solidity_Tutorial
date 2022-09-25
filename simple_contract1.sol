65e78441230ec6a09c3aef638ae7bd3f96cc44b3
Kamal Joshi <kamal_j@ch.iitr.ac.in>
2022-09-25 16:22:13 +0530

    simple_contract1.sol

---
+++ simple_contract1.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+//  pragma solidity 0.8.7  // pragma tells the current solidity version
+pragma solidity ^0.8.7; // this tells that we are okay with 8.7 and above version of solidity
+// tells that any version between the range is okay to work
+contract simplestorage{
+    uint public favoritenumber;
+    function store(uint _favoritenumber) public{
+        favoritenumber=_favoritenumber;
+        // retrieve();
+    }
+    
+    mapping(string => uint256) public nameToage; // mapping ia a data structure int which a key points to a value . Here nameTopage becomes a dictionary that holds the key-value pair.
+    // mapping is introduced to get the value if we know a particular key then we can easily get the value by mapping. Otherwise we would have iterated the whole array to get the value. 
+
+    // view, pure are the keywords in which you dont need to spend any gas
+    function retrieve() public view returns(uint){
+        return favoritenumber;
+    }
+    struct person{
+        uint age;
+        string name;
+    }
+    person[] public persons;
+    function  addPerson(string memory _name, uint _age) public {
+         // memory keyword is actually adding the data in the memory
+        persons.push(person(_age,_name)); // pushing values int the array   
+        // another method first create a peoples then add in the array
+        // person memory newperson=person({age:_age,name:_name});
+        // persons.push(newperson);
+        // In solidity we need to specify the memory for array , structure, mapping and string is considered as array that's why memory is assigned to it
+        // calldata is temporary variables that cannot be modidfied, memory is a temporary variables that can be modidified and storage is a permanent variable that cannot be modified
+        nameToage[_name]=_age;
+    }
+}
